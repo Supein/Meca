128059a253f3a29b5bb5aece3be0f78757b7522a
IronMyke <miguelmatias93@gmail.com>
2016-04-02 19:24:19 +0200

    Asunto

--- demo/models.py
+++ demo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class GameReservation(models.Model):
 	"""docstring for GameReservation"""
-	tokenId = models.CharField(max_length=10)
+	player = models.ForeignKey(Player)
 	invitationId = models.CharField(max_length=10)
 	promotionId = models.BooleanField(default=True)
 	def __unicode__(self):
@@ -15,4 +15,70 @@ class GameRequest(models.Model):
 	validationId = models.CharField(max_length=10)
 	invitaionId = models.CharField(max_length=10)
 
-class ActiveGame(models.Model):
+class Player(models.Model):
+	tokenId = models.CharField(max_length=10,  primary_key=True)
+	creationDate = models.DateField(default=datetime.now())
+
+class Image(models.Model):
+	"""docstring for Image"""
+	solution = models.CharField(max_length=12)
+	rawFile = models.FileField(upload_to='rawImages/')
+		
+class ImageChallenge(object):
+	"""docstring for ImageChallenge"""
+	image = models.ForeignKey(Image)
+	game = models.ForeignKey(Game)
+
+	startTime = models.DateField(default=datetime.now())
+	timeout = timedelta(seconds=20)
+	endTime = models.DateField(default=Field.null)
+	def isActive():
+		if endTime != null:
+			return false
+		else if !self.game.isActive():
+			self.end()
+			return false
+		else if datetime.now()>(self.startTime+timeout):
+			self.end()
+			return false
+		else:
+			return true
+	def end()
+		self.endTime = datetime.now()
+		
+
+class Game(models.Model):
+	request = models.ForeignKey(GameRequest)
+	reservation = models.ForeignKey(GameReservation)
+
+	startTime = models.DateField(default=datetime.now())
+	timeout = timedelta(seconds=60)
+	endTime = models.DateField(default=Field.null)
+
+	activeChallenge = models.ForeignKey(ImageChallenge)
+
+	def isActive():
+		if endTime != null:
+			return false
+		else if datetime.now() > (self.startTime + timeout):
+			self.end()
+			return false
+		else:
+			return true
+
+	def end()
+		self.endTime = datetime.now()
+		self.activeChallenge.end()
+
+
+
+
+
+
+
+
+
+
+
+
+
--- demo/views.py
+++ demo/views.py
@@ -13,13 +13,23 @@ def gameReservation(request):
 	response=HttpResponse()
 
 	if (tokenId !='' and invitationId != '' and promotionId != ''):
-		reservation = GameReservation(tokenId=tokenId, invitationId=invitationId, promotionId=promotionId)
-		reservation.save()
+		storedReservation = GameReservation(tokenId=tokenId, invitationId=invitationId, promotionId=promotionId)
+		storedReservation.save()
 		response.status_code=200
 	else:
 		response.status_code=400
 	return response
 
 def gameRequest(request):
-	template = loader.get_template('demo.html')
-	return HttpResponse(template.render(request))
+	validationId=request.REQUEST.get('validation','')
+	invitationId=request.REQUEST.get('invitation','')
+
+	response=HttpResponse()
+
+	if (validationId !='' and invitationId != '' and promotionId != ''):
+		storedRequest = GameReservation(validationId=validationId, invitationId=invitationId, promotionId=promotionId)
+		storedRequest.save()
+		response.status_code=200
+	else:
+		response.status_code=400
+	return response
